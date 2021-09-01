2887ba1dc71462962ee7fb7e4423ffcb791af37a
Sk Imran Sabuj <sabujx9@gmail.com>
2021-09-02 04:39:41 +0600

    second commit

--- js/book.js
+++ js/book.js
@@ -1,46 +1,50 @@
 const searchBookByName = () => {
     const serachInput = document.getElementById('search-input');
     const seracahValue = serachInput.value;
-    if (seracahValue != '') {
+    if (seracahValue !== '') {
         // clear data 
         serachInput.value = "";
-        const url = `http://openlibrary.org/search.json?q=${seracahValue}`;
         // load data
+        const url = `http://openlibrary.org/search.json?q=${seracahValue}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySearchResult(data.docs,seracahValue))
+            .then(data => displaySearchResult(data.docs, seracahValue))
+    }
+    else {
+        document.getElementById('result-amount').innerHTML = `
+            <h3 class="result-amount-bar">Please write book name or author name then search</h3>
+        `;
     }
 }
-// .then(data => displaySearchResult(data.meals))
-//search result section
-const displaySearchResult = (books,seracahValue) => {
+const displaySearchResult = (books, seracahValue) => {
     console.log(books);
-    const searchResult = document.getElementById('search-result');
+
+    // Show search amount or message  
     const searchDiv = document.getElementById('result-amount');
+    searchDiv.innerHTML = '';
     const h3 = document.createElement('h3');
-    h3.classList.add('text-center');
+    h3.classList.add('result-amount-bar');
     h3.innerText = `${books.length} Results found containg '${seracahValue}'`;
     searchDiv.appendChild(h3);
-    // searchResult.innerHTML='';
+    // show search Result 
+    const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     books.forEach(book => {
-        // console.log(meals)
         const div = document.createElement('div');
-
         div.classList.add('col');
-        let publisher='';
-        book.publisher.forEach(pub =>{
-            publisher=publisher+pub+' ';
-        });
+        // let publisher='';
+        // book.publisher.forEach(pub =>{
+        //     publisher=publisher+pub+' ';
+        // });
         div.innerHTML = `
             <div onclick="loadMealDetails(${book.idMeal})" class="card" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <h3 class="card-title">${book.title}</h3>
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="...">
+                
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="Image Not Found">
                 <div class="card-body">
-                    
-                    <h5 class="card-text">First published: ${book.first_publish_year}</h5>
-                    <h6 class="card-text">Author Name: ${book.author_name}</h6>
-                    <h6 class="card-text">Publishers: ${book.publisher}</h6>
+                    <h3 class="card-title text-center text-success mb-3">${book.title}</h3>
+                    <h6 class="card-text"><b>Author Name:</b> ${book.author_name}</h6>
+                    <h6 class="card-text"><b>First Published:</b> ${book.first_publish_year} </h6>
+                    <p class="card-text"><b>Publishers: </b> ${book.publisher}</p>
                 </div>
             </div>
         `;
