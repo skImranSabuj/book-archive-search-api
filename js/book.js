d1c085ac8246e7e711c544a95e5d17cb9f277c76
Sk Imran Sabuj <sabujx9@gmail.com>
2021-09-02 18:08:30 +0600

    fifth commint

--- js/book.js
+++ js/book.js
@@ -22,7 +22,7 @@ const displaySearchResult = (books, seracahValue) => {
     // Show search amount or message  
     const searchDiv = document.getElementById('result-amount');
     searchDiv.innerHTML = '';
-    
+
     const found = document.createElement('h3');
     found.classList.add('result-amount-bar');
     found.innerText = `${books.numFound} Results found containg '${seracahValue}'`;
@@ -41,17 +41,23 @@ const displaySearchResult = (books, seracahValue) => {
         if (book.author_name === undefined) book.author_name = ['Unknown Author'];
         if (book.first_publish_year === undefined) book.first_publish_year = 'Unknown';
         if (book.publisher === undefined) book.publisher = ['No Publisher found'];
+        let imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+        if (book.cover_i === undefined) {
+            imgUrl = `images/cover-null.jpg`;
+        }
         // loading innerHtmal for search result
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div class="card shadow-lg" data-bs-toggle="modal" data-bs-target="#exampleModal" >
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top p-3" alt="Image Not Found">
+            <div class="card shadow-lg border-lelt border-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                <img src=${imgUrl} class="card-img-top p-3" alt="Image Not Found">
                 <div class="card-body">
-                    <h5 class="card-title mb-3">Book Name:<b class="text-success"> ${book.title.slice(0, 100)}</b></h5>
-                    <h6 class="card-text"><b class="text-success">Author Name: </b>${book.author_name[0]}</h6>
-                    <h6 class="card-text"><b class="text-success">First Published:</b> ${book.first_publish_year} </h6>
-                    <p class="card-text"><b class="text-success">Publishers: </b> ${book.publisher[0]}</p>
+                    <h5 class="card-title mb-3 bg-light p-2 rounded"><b class="text-success">Book Name: </b> ${book.title.slice(0, 100)}</h5>
+                    <div class="details bg-light p-2 rounded">
+                        <h6 class="card-text"><b class="text-success">Author Name: </b>${book.author_name[0]}</h6>
+                        <h6 class="card-text"><b class="text-success">First Published:</b> ${book.first_publish_year} </h6>
+                        <p class="card-text"><b class="text-success">Publishers: </b> ${book.publisher[0]}</p>
+                    </div>
                 </div>
             </div>
     `;
