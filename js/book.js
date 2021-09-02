9a2bbb913cda95d3ef58e80bf534b6071f3f50af
Sk Imran Sabuj <sabujx9@gmail.com>
2021-09-02 15:26:05 +0600

    third  commit

--- js/book.js
+++ js/book.js
@@ -5,10 +5,10 @@ const searchBookByName = () => {
         // clear data 
         serachInput.value = "";
         // load data
-        const url = `http://openlibrary.org/search.json?q=${seracahValue}`;
+        const url = `https://openlibrary.org/search.json?q=${seracahValue}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySearchResult(data.docs, seracahValue))
+            .then(data => displaySearchResult(data, seracahValue))
     }
     else {
         document.getElementById('result-amount').innerHTML = `
@@ -24,12 +24,13 @@ const displaySearchResult = (books, seracahValue) => {
     searchDiv.innerHTML = '';
     const h3 = document.createElement('h3');
     h3.classList.add('result-amount-bar');
-    h3.innerText = `${books.length} Results found containg '${seracahValue}'`;
+    h3.innerText = `${books.numFound} Results found containg '${seracahValue}'
+    ${books.docs.length} Result shown`;
     searchDiv.appendChild(h3);
     // show search Result 
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    books.forEach(book => {
+    books.docs.forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
         // let publisher='';
@@ -37,17 +38,22 @@ const displaySearchResult = (books, seracahValue) => {
         //     publisher=publisher+pub+' ';
         // });
         div.innerHTML = `
-            <div onclick="loadMealDetails(${book.idMeal})" class="card" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="Image Not Found">
+        <div class="card" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="Image Not Found">
                 <div class="card-body">
                     <h3 class="card-title text-center text-success mb-3">${book.title}</h3>
-                    <h6 class="card-text"><b>Author Name:</b> ${book.author_name}</h6>
-                    <h6 class="card-text"><b>First Published:</b> ${book.first_publish_year} </h6>
-                    <p class="card-text"><b>Publishers: </b> ${book.publisher}</p>
+                    <h6 class="card-text"><b class="text-primary">Author Name:</b> ${book.author_name}</h6>
+                    <h6 class="card-text"><b class="text-warning">First Published:</b> ${book.first_publish_year} </h6>
+                    <p class="card-text"><b class="text-info">Publishers: </b> ${book.publisher}</p>
                 </div>
             </div>
-        `;
+    `;
         searchResult.appendChild(div);
     })
+}
+const getCover = (cover_i) => {
+    const url = `https://covers.openlibrary.org/b/id/${cover_i}-L.jpg`;
+    if (true) {
+        console.log(typeof url);
+    }
 }
